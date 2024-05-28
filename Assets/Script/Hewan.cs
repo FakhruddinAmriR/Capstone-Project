72630f3309af6697014cf3ff5fb4cfff19ea0ef3
Fakhruddin Amri Rakhmatullah <fakhruddinamri5@gmail.com>
2024-05-28 15:10:39 +0700

    add player can click and get achievment (without system save and load)

---
+++ Assets/Script/Hewan.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hewan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject panel;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero);
+            if (hit.collider != null & hit.collider.gameObject == gameObject)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0f;
+                Debug.Log("Tersentuh");
+
+                panel.SetActive(true);
+            }
+        }  
+    }
+}
