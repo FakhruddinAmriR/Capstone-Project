6c25b78a6619649d5543c3abdbf6a712e894240d
Fakhruddin Amri Rakhmatullah <fakhruddinamri5@gmail.com>
2024-07-08 11:08:46 +0700

    FIX BANGET BUAT CAPSTONE

--- Assets/Script/ButtonMainMenu.cs
+++ Assets/Script/ButtonMainMenu.cs
@@ -1,6 +1,6 @@
+
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -18,5 +18,12 @@ public class ButtonMainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    
+
 }
+
+
+
+
+
+
+
